0a7b521a022af9bc7a2635e50e864324f6a571d1
naveen ajay kumar Nagineni <naveen.nagineni6666@gmail.com>
2024-05-17 19:43:52 -0500

    data transformation done

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_requirements(file_path:str)->List[str]:
 
 setup(
 name='ml_project_1',
-version='0.0.1'
+version='0.0.1',
 author='Naveen',
 author_email='naveen.nagineni6666@gmail.com',
 packages=find_packages(),
